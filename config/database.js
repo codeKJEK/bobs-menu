280e05ee555bd82da1be9e598e7dade1df631995
KJEKcode <kurtiskovel@gmail.com>
2021-08-01 14:52:21 -0400

    ssl

--- config/database.js
+++ config/database.js
@@ -1,44 +1,68 @@
-const parse = require("pg-connection-string").parse;
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
 
-
-module.exports = ({ env }) => {
-  if(env("NODE_ENV") === "production") {
-    const config = parse(process.env.DATABASE_URL)
-    return {
-      defaultConnection: "default",
-      connections: {
-        default: {
-          connectior: "bookshelf",
-          settings: {
-            client: "postgres",
-            host: config.host,
-            port: config.port,
-            database:config.database,
-            username: config.user,
-            password: config.password
-          },
-          options: {
-            ssl: {
-              rejectUnauthorized: false
-            }
-        }
-      }
-    }
-  }
-  
-  return {
-    defaultConnection: 'default',
-    connections: {
-      default: {
-        connector: 'bookshelf',
-        settings: {
-          client: 'sqlite',
-          filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-        },
-        options: {
-          useNullAsDefault: true,
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
+        ssl: {
+          rejectUnauthorized: false,
         },
       },
+      options: {
+        ssl: true,
+      },
     },
-  }
-};
+  },
+});
+// const parse = require("pg-connection-string").parse;
+
+
+// module.exports = ({ env }) => {
+//   if(env("NODE_ENV") === "production") {
+//     const config = parse(process.env.DATABASE_URL)
+//     return {
+//       defaultConnection: "default",
+//       connections: {
+//         default: {
+//           connectior: "bookshelf",
+//           settings: {
+//             client: "postgres",
+//             host: config.host,
+//             port: config.port,
+//             database:config.database,
+//             username: config.user,
+//             password: config.password
+//           },
+//           options: {
+//             ssl: {
+//               rejectUnauthorized: false
+//             }
+//         }
+//       }
+//     }
+//   }
+// }
+//   return {
+//     defaultConnection: 'default',
+//     connections: {
+//       default: {
+//         connector: 'bookshelf',
+//         settings: {
+//           client: 'sqlite',
+//           filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+//         },
+//         options: {
+//           useNullAsDefault: true,
+//         },
+//       },
+//     },
+//   };
