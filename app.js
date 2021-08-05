7d40a00fc2ea45de2f02cdabe3e5e6fd587db04a
KJEKcode <kurtiskovel@gmail.com>
2021-08-05 17:24:21 -0400

    clean up app.js

--- app.js
+++ app.js
@@ -1,120 +1,160 @@
-window.addEventListener("DOMContentLoaded", () => {
+async function init() {
+    const response = await fetch(
+        "https://bobs-family-midland-menu.herokuapp.com/products?_sort=name:asc"
+    );
+    const products = await response.json();
+
+    return products;
+}
+
+window.addEventListener("DOMContentLoaded", async () => {
+  // recieve products
+
+    let products = [];
+    try {
+        products = await init();
+    } catch (e) {
+        console.log("ERROR!");
+        console.log(e);
+    }
+
+  // get all the categories
+
+    const categories = [];
+    products.forEach((product) => {
+        if (!categories.includes(product.category)) {
+            categories.push(product.category);
+        }
+    });
+
+  // create and diplay menu category
+
+    categories.forEach((category) => {
+        const singleCategory = products.filter(
+            (product) => product.category === category
+        );
+        let categoryHTML = singleCategory.map((item) => {
+            let desc = ``;
+            if (item.description == null) {
+            desc = ``;
+            } else {
+            desc = `<p>${item.description}</p>`;
+            }
+            return `
+            <article>
+                <header class="article-header">
+                    <h3>${item.name}</h3>
+                    <h3>${(
+                        Math.round(item.price * 100) / 100
+                    ).toFixed(2)}</h3>
+                </header>
+                ${desc}
+            </article>`;
+        });
+        categoryHTML = categoryHTML.join("");
+        let inlineNav = `
+        <ul>
+        <li><button data-category="${category}" data-action="${category}-click-filter" class="filter-btn"><figure data-category="${category}" data-action="${category}-click-filter"><img src="uploads/filter-icon-dark.svg" alt="filter" data-category="${category}" data-action="${category}-click-filter"
+        data-category="${category}" data-action="${category}-click-menu"></figure>filter</button></li>
+        <li><button data-category="${category}" data-action="${category}-click-menu" class="menu-btn"><figure data-category="${category}" data-action="${category}-click-menu"><img src="uploads/menu-icon-dark.svg" alt="menu" data-category="${category}" data-action="${category}-click-menu"></figure>menu</button></li>
+        <li class="call-cta"><a href="tel:+7048883898" data-category="${category}" data-action="${category}-click-call"><figure data-category="${category}" data-action="${category}-click-call"><img src="uploads/call-icon-dark.svg" alt="call" data-category="${category}" data-action="${category}-click-call"></figure>call</a></li>
+        </ul>
+        `
+        categoryHTML += inlineNav;
+        document.getElementById(category).querySelector("section").innerHTML = categoryHTML;
+    });
+
+    //filter button click event listener
+
+    const filterBtns = document.querySelectorAll(".filter-btn"),
+    menuBtns = document.querySelectorAll(".menu-btn");
+    filterBtns.forEach(btn => btn.addEventListener("click", () => {
+        asideMenu.setAttribute("aria-hidden", "false");
+        inlineMenu.hidden = true;
+        filterMenu.hidden = false;
+        menuOption.classList.remove("active");
+        filterOption.classList.add("active");
+
+    }))
+
+    // menu button click event listener
+
+    menuBtns.forEach(btn => btn.addEventListener("click", () => {
+        asideMenu.setAttribute("aria-hidden", "false");
+        filterMenu.hidden = true;
+        inlineMenu.hidden = false;
+        filterOption.classList.remove("active");
+        menuOption.classList.add("active");
+    }))
+
+    // close menu function
+
     const asideMenu = document.getElementById("app-aside"),
     inlineMenu = document.getElementById("inline-menu"),
-    inlineCtas = inlineMenu.querySelectorAll("a"),
-    filterMenu = document.getElementById("filter-menu"),
-    checkBoxes = filterMenu.querySelectorAll("input"),
-    closeBtns = document.querySelectorAll(".close-btn"),
-    clearFilter = document.querySelector(".clear-filter"),
-    menuOption = document.getElementById("menu-option"),
-    filterOption = document.getElementById("filter-option"),
-    menuSections = document.querySelectorAll(".menu-section"),
-    menuItems = document.querySelectorAll(".menu-item"),
-    sideItems = document.querySelectorAll(".side-item");
-    
-    fetch("https://bobs-family-midland-menu.herokuapp.com/products?_sort=name:asc")
-    .then(res => {
-        if (res.ok) {
-            return res.json();
-        } else {
-            console.log("ERROR: Network Error or No Product Found")
-        }
-    })
-    .then(data => {
-        console.log(data);
-        let allCategories = [];
-        data.forEach(product => {
-            if(!allCategories.includes(product.category)) {
-                allCategories.push(product.category);
-            }
-        });
-        allCategories.forEach(category => {
-            let singleCategory = data.filter(product => product.category == category);
-            let categoryHTML = singleCategory.map(item => {
-                let desc = ``
-                if (item.description == null) {
-                    desc = ``
-                } else {
-                    desc = `<p>${item.description}</p>`
-                }
-                return `
-                <article>
-                    <header class="article-header">
-                        <h3>${item.name}</h3>
-                        <h3>${(Math.round(item.price * 100) / 100).toFixed(2)}</h3>
-                    </header>
-                    ${desc}
-                </article>`
-            });
-            categoryHTML = categoryHTML.join("");
-            let inlineNav = `
-            <ul>
-            <li><button data-category="${category}" data-action="${category}-click-filter" class="filter-btn"><figure data-category="${category}" data-action="${category}-click-filter"><img src="uploads/filter-icon-dark.svg" alt="filter" data-category="${category}" data-action="${category}-click-filter"
-            data-category="${category}" data-action="${category}-click-menu"></figure>filter</button></li>
-            <li><button data-category="${category}" data-action="${category}-click-menu" class="menu-btn"><figure data-category="${category}" data-action="${category}-click-menu"><img src="uploads/menu-icon-dark.svg" alt="menu" data-category="${category}" data-action="${category}-click-menu"></figure>menu</button></li>
-            <li class="call-cta"><a href="tel:+7048883898" data-category="${category}" data-action="${category}-click-call"><figure data-category="${category}" data-action="${category}-click-call"><img src="uploads/call-icon-dark.svg" alt="call" data-category="${category}" data-action="${category}-click-call"></figure>call</a></li>
-            </ul>
-            `
-            categoryHTML += inlineNav;
-            document.getElementById(category).insertAdjacentHTML('beforeend', categoryHTML);
-        })
-        const filterBtns = document.querySelectorAll(".filter-btn"),
-        menuBtns = document.querySelectorAll(".menu-btn");
-        filterBtns.forEach(btn => btn.addEventListener("click", () => {
-            asideMenu.setAttribute("aria-hidden", "false");
-            inlineMenu.hidden = true;
-            filterMenu.hidden = false;
-            menuOption.classList.remove("active");
-            filterOption.classList.add("active");
-            
-        }))
-        menuBtns.forEach(btn => btn.addEventListener("click", () => {
-            asideMenu.setAttribute("aria-hidden", "false");
-            filterMenu.hidden = true;
-            inlineMenu.hidden = false;
-            filterOption.classList.remove("active");
-            menuOption.classList.add("active");
-        }))
-    })
+    filterMenu = document.getElementById("filter-menu");
     function closeMenu() {
         asideMenu.setAttribute("aria-hidden", "true");
         inlineMenu.hidden = true;
         filterMenu.hidden = true;
     }
+
+    // close button click event listener
+
+    const closeBtns = document.querySelectorAll(".close-btn");
     closeBtns.forEach(btn => btn.addEventListener("click", () => {
         closeMenu();
     }))
+
+    // side menu cta click event listener 
+
+    const inlineCtas = inlineMenu.querySelectorAll("a");
     inlineCtas.forEach(cta => cta.addEventListener("click", () => {
         if(window.innerWidth < 769){
             closeMenu();
         }
-        
+
     }))
+
+    // aside menu click event listener
+
     asideMenu.addEventListener("click", (event) => {
         if(event.target === asideMenu) {
             console.log(event.target)
             closeMenu();
         }
     })
+
+    // menu option click event listener
+
+    const menuOption = document.getElementById("menu-option"),
+    filterOption = document.getElementById("filter-option");
     menuOption.addEventListener("click", () => {
         filterMenu.hidden = true;
         inlineMenu.hidden = false;
         filterOption.classList.remove("active");
         menuOption.classList.add("active");
     })
+
+    // filter option click event listener
     filterOption.addEventListener("click", () => {
         filterMenu.hidden = false;
         inlineMenu.hidden = true;
         menuOption.classList.remove("active");
         filterOption.classList.add("active");
     })
-    
+
+    // filter change event listener
+
+    const checkBoxes = filterMenu.querySelectorAll("input"),
+    clearFilter = document.querySelector(".clear-filter"),
+    menuSections = document.querySelectorAll(".menu-section"),
+    menuItems = document.querySelectorAll(".menu-item"),
+    sideItems = document.querySelectorAll(".side-item");
     checkBoxes.forEach(checkBox => checkBox.addEventListener("change", () => {
         let numSelected = 0;
         let filterSection = [];
         let filterMenu = [];
         let filterAside = [];
-        
         checkBoxes.forEach(input => {
             if (input.checked === true) {
                 numSelected++
@@ -148,6 +188,7 @@ window.addEventListener("DOMContentLoaded", () => {
                 }
             })
         } else {
+            clearFilter.classList.remove("color");
             menuSections.forEach(section => {
                 section.hidden = false;
             })
@@ -159,6 +200,9 @@ window.addEventListener("DOMContentLoaded", () => {
             })
         }
     }))
+
+    // clear filter click event listener
+
     clearFilter.addEventListener("click", () => {
         clearFilter.classList.remove("color");
         checkBoxes.forEach(checkbox => {
@@ -174,6 +218,4 @@ window.addEventListener("DOMContentLoaded", () => {
             item.hidden = false;
         })
     })
-})
-
-
+});
