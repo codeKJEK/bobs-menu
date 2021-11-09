153eeae85b69f04a7178630a1dab03d6d108694a
KJEKcode <kurtiskovel@gmail.com>
2021-11-08 19:41:47 -0500

    hide

--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '4ba6478cfbe1795f06f02db0aabb0622'),
+      secret: env('ADMIN_JWT_SECRET'),
     },
   },
 });
